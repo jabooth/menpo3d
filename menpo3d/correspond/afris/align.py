d6346bc4cf5219a9bf1651932c81341e7c2f9154
James Booth <jabooth@gmail.com>
2015-01-16 16:43:22 +0000

    remove dependency on from_mask behavior that we are removing

--- menpo3d/correspond/afris/align.py
+++ menpo3d/correspond/afris/align.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 from menpo.transform import AlignmentSimilarity
-
+from menpo.shape import PointCloud
 
 AlignFRResult = namedtuple('AlignFRResult',
                            ['sparse_3d', 'texture_image', 'shape_image'])
@@ -39,9 +39,14 @@ class LandmarkAFR(AlignFR):
 class PartialLandmarkAFR(AlignFR):
 
     def __call__(self, mesh, mask, group=None, label=None):
+        # copy the template
+        masked_sparse_template_3d = PointCloud(self.sparse_template_3d.points)
+        # filter the points based on the mask
+        masked_sparse_template_3d.points = masked_sparse_template_3d.points[mask, :]
+
         alignment = AlignmentSimilarity(
             mesh.landmarks[group][label],
-            self.sparse_template_3d.from_mask(mask)).as_non_alignment()
+            masked_sparse_template_3d).as_non_alignment()
         aligned_mesh = alignment.apply(mesh)
         result = self.fr(aligned_mesh)
         result['sparse_3d'] = aligned_mesh.landmarks[group][label]
