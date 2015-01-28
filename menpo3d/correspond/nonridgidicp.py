c6afbb5ec811a248a9bd5292b69e3cb3d81f2c4c
James Booth <jabooth@gmail.com>
2015-01-28 08:19:38 +0000

    add weighting for normals.

--- menpo3d/correspond/nonridgidicp.py
+++ menpo3d/correspond/nonridgidicp.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.sparse.linalg import spsolve
 import scipy.sparse as sp
 import vtk
+from menpo.shape import TriMesh
 
 
 def build_intersector(mesh):
@@ -38,7 +39,7 @@ def build_closest_point_locator(mesh):
     def closest_point(point):
         cell_locator.FindClosestPoint(point, c_point,
                                       cell_id, sub_id, distance)
-        return c_point[:], cell_id.get(), distance.get()
+        return c_point[:], cell_id.get()
 
     return closest_point
 
@@ -94,9 +95,11 @@ def non_rigid_icp(source, target, eps=1e-3):
     print('building nearest point locator for target...')
     closest_point_on_target = build_closest_point_locator(target_vtk)
 
+    # save out the target normals. We need them for the weight matrix.
+    target_tri_normals = target.face_normals()
+
     # init transformation
-    X_prev = np.zeros((n_dims, n_dims + 1))
-    X_prev = np.tile(X_prev, n).T
+    X_prev = np.tile(np.zeros((n_dims, h_dims)), n).T
     v_i = points
 
     # start nicp
@@ -123,15 +126,40 @@ def non_rigid_icp(source, target, eps=1e-3):
 
         # iterate until X converge
         while True:
-            # find nearest neighbour
-            U = np.array([closest_point_on_target(p)[0]
-                          for p in v_i])
+
+            # find nearest neighbour and the normals
+            points = []
+            tri_indicies = []
+            for p in v_i:
+                point, tri_index = closest_point_on_target(p)
+                points.append(point)
+                tri_indicies.append(tri_index)
+            U = np.array(points)
+
+
+            u_i_n = target_tri_normals[tri_indicies]
+
+            # calculate the normals of the current v_i
+            v_i_n = TriMesh(v_i, trilist=trilist, copy=False).vertex_normals()
+
+            # If the dot of the normals is lt 0.9 don't contrib to deformation
+            w_i_n = (u_i_n * v_i_n).sum(axis=1) > 0.9
+
+            # Form the overall w_i from the normals, edge case and self
+            # intersection
+            w_i = w_i_n
+
+            # Build the sparse diagonal weight matrix
+            W_s = sp.diags(w_i.astype(np.float)[None, :], [0])
 
 
             data = np.hstack((v_i.ravel(), o))
             D_s = sp.coo_matrix((data, (row, col)))
 
-            A_s = sp.vstack((alpha_M_kron_G_s, D_s)).tocsr()
+            # nullify the masked U values
+            U[~w_i] = 0
+
+            A_s = sp.vstack((alpha_M_kron_G_s, W_s.dot(D_s))).tocsr()
             B_s = sp.vstack((np.zeros((alpha_M_kron_G_s.shape[0], n_dims)),
                              U)).tocsr()
             X_s = spsolve(A_s.T.dot(A_s), A_s.T.dot(B_s))
@@ -149,4 +177,6 @@ def non_rigid_icp(source, target, eps=1e-3):
     # final result
     point_corr = np.array([closest_point_on_target(p)[0]
                            for p in v_i])
-    return v_i, point_corr
+    # only update the points for the non-problematic ones
+    v_i[w_i] = point_corr[w_i]
+    return v_i
