2eae47737e307e9189141adeaf830795afc8b105
James Booth <jabooth@gmail.com>
2017-01-12 08:45:07 +0000

    adapt menpo stype for lambertian lighting

--- menpo3d/rasterize/shading.py
+++ menpo3d/rasterize/shading.py
@@ -1,9 +1,6 @@
 import numpy as np
-from menpo.shape import TriMesh, ColouredTriMesh
 from menpo.transform import UniformScale, Translation
 
-__all__ = ["LambertianModel"]
-__default_light_sources__ = ((.667, -.66, .33), (-.667, .66, .33))
 
 def l2_normalize(x, axis=0, epsilon=1e-12):
     """
@@ -24,16 +21,21 @@ def l2_normalize(x, axis=0, epsilon=1e-12):
     """
     return x / np.maximum(np.linalg.norm(x, axis=axis), epsilon)
 
+
 def mesh_in_a_sphere(mesh):
     scale = UniformScale(1 / mesh.norm(), mesh.n_dims)
-
     translation = Translation(-scale.apply(mesh).centre())
     return translation.compose_after(scale)
 
+
 class LambertianModel(object):
-    def __init__(self, diffuse_colour=(.5, .5, .5), light_positions=__default_light_sources__):
+
+    def __init__(self, diffuse_colour=(.5, .5, .5),
+                 light_positions=((.667, -.66, .33),
+                                  (-.667, .66, .33))):
         self.diffuse_colour = np.asarray(diffuse_colour)
-        self.light_positions = l2_normalize(np.asarray(light_positions).reshape(-1, 3), axis=0)
+        self.light_positions = l2_normalize(
+            np.asarray(light_positions).reshape(-1, 3), axis=0)
 
     def apply(self, mesh, use_texture=False, copy=True):
         mesh = mesh.as_colouredtrimesh(copy=copy)
@@ -41,11 +43,13 @@ class LambertianModel(object):
         unit_transform = mesh_in_a_sphere(mesh)
         mesh = unit_transform.apply(mesh)
         light_directions = l2_normalize(
-            self.light_positions.reshape(-1, 1, 3) - mesh.points[None, ...], axis=0)
+            self.light_positions.reshape(-1, 1, 3) - mesh.points[None, ...],
+            axis=0)
         
-        # Calculate the lambertian reflectance for each light source.
-        # This will be an `ndarray` of dimensions num_light_sources x num_vertices.
-        lambertian = np.sum(light_directions * mesh.vertex_normals()[None, ...], 2)[..., None]
+        # Calculate the Lambertian reflectance for each light source.
+        # This will be an `ndarray` of shape(num_light_sources, num_vertices)
+        lambertian = np.sum(light_directions *
+                            mesh.vertex_normals()[None, ...], 2)[..., None]
 
         # Sum up the contribution of all the light sources and multiply by the
         # diffusion colour.
