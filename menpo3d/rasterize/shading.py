c18fea1bd594e3bfe1ca5263e1b2a80dd9f7fcb9
James Booth <jabooth@gmail.com>
2017-01-12 09:17:17 +0000

    move to single function, more readable defaults

--- menpo3d/rasterize/shading.py
+++ menpo3d/rasterize/shading.py
@@ -1,5 +1,6 @@
 import numpy as np
 from menpo.transform import UniformScale, Translation
+from menpo3d.result import as_colouredtrimesh
 
 
 def l2_normalize(x, axis=0, epsilon=1e-12):
@@ -22,41 +23,36 @@ def l2_normalize(x, axis=0, epsilon=1e-12):
     return x / np.maximum(np.linalg.norm(x, axis=axis), epsilon)
 
 
-def mesh_in_a_sphere(mesh):
+def mesh_in_unit_sphere(mesh):
     scale = UniformScale(1 / mesh.norm(), mesh.n_dims)
     translation = Translation(-scale.apply(mesh).centre())
     return translation.compose_after(scale)
 
 
-class LambertianModel(object):
+def lambertian_shading(mesh, diffuse_colour=(.5, .5, .5),
+                       light_positions=((2, -2, 1), (-2, 2, 1))):
 
-    def __init__(self, diffuse_colour=(.5, .5, .5),
-                 light_positions=((.667, -.66, .33),
-                                  (-.667, .66, .33))):
-        self.diffuse_colour = np.asarray(diffuse_colour)
-        self.light_positions = l2_normalize(
-            np.asarray(light_positions).reshape(-1, 3), axis=0)
+    diffuse_colour = np.asarray(diffuse_colour)
+    light_positions = l2_normalize(np.asarray(light_positions).reshape(-1, 3),
+                                   axis=0)
 
-    def apply(self, mesh, use_texture=False, copy=True):
-        mesh = mesh.as_colouredtrimesh(copy=copy)
-            
-        unit_transform = mesh_in_a_sphere(mesh)
-        mesh = unit_transform.apply(mesh)
-        light_directions = l2_normalize(
-            self.light_positions.reshape(-1, 1, 3) - mesh.points[None, ...],
-            axis=0)
-        
-        # Calculate the Lambertian reflectance for each light source.
-        # This will be an `ndarray` of shape(num_light_sources, num_vertices)
-        lambertian = np.sum(light_directions *
-                            mesh.vertex_normals()[None, ...], 2)[..., None]
+    mesh = as_colouredtrimesh(mesh)
 
-        # Sum up the contribution of all the light sources and multiply by the
-        # diffusion colour.
-        lambertian = lambertian.sum(0) * self.diffuse_colour
+    unit_transform = mesh_in_unit_sphere(mesh)
+    mesh = unit_transform.apply(mesh)
 
-        texture = mesh.colours if use_texture else 0
-        mesh.colours[..., :] = texture + lambertian
-        mesh.colours[...] = np.clip(mesh.colours, 0, 1)
+    light_directions = l2_normalize(light_positions.reshape(-1, 1, 3) -
+                                    mesh.points[None, ...], axis=0)
 
-        return unit_transform.pseudoinverse().apply(mesh)
+    # Calculate the lambertian reflectance for each light source.
+    # This will be an `ndarray` of shape(num_light_sources, num_vertices)
+    lambertian = np.sum(light_directions *
+                        mesh.vertex_normals()[None, ...], 2)[..., None]
+
+    # Sum up the contribution of all the light sources and multiply by the
+    # diffusion colour.
+    lambertian = lambertian.sum(0) * diffuse_colour
+
+    mesh.colours[...] = np.clip(mesh.colours + lambertian, 0, 1)
+
+    return unit_transform.pseudoinverse().apply(mesh)
