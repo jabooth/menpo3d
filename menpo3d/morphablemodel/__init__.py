4d0351ec594de6c0e58c98cc7bd5a65a8a136b78
James Booth <jabooth@gmail.com>
2018-02-12 18:49:36 +0000

    expose top level mm API

--- menpo3d/morphablemodel/__init__.py
+++ menpo3d/morphablemodel/__init__.py
@@ -1,3 +1,8 @@
 from .base import ColouredMorphableModel, TexturedMorphableModel
 from .algorithm import ProjectOutForwardAdditive
 from .fitter import LucasKanadeMMFitter
+from .video import (
+    fit_image, fit_video, instance_for_params,
+    render_initialization, render_iteration,
+    generate_person_specific_texture_model
+)
